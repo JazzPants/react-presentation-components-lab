260435b13c5e04bcefe379f138f036463d0f20e9
JazzPants <leighton.l14@gmail.com>
2022-01-10 01:21:46 +1100

    Done! Added notes1

--- src/index.js
+++ src/index.js
@@ -11,3 +11,12 @@ ReactDOM.render(
   </div>,
   document.getElementById('root')
 );
+
+//if we wanted a more complex function, we could use this.handleClick and right the function
+//in the parent container component (which may contain state)
+//can also keep state in that parent container component which makes debugging/making changes easier
+
+//if you can't make the stateless functional component work with the "container-held" state
+//then change it to a full React Component with state
+
+//Start from smallest blocks/components then scale up
