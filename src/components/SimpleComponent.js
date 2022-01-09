c2dec5460284052fbd1235dbddcff5de159823eb
JazzPants <leighton.l14@gmail.com>
2022-01-10 01:17:17 +1100

    Done!

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,33 @@
 // Code SimpleComponent Here
+import React, {Component} from 'react'
+
+class SimpleComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            mood: "happy"
+        }
+    }
+    //must use arrow functions, can't use handleClick() {...} 
+    handleClick = () => {
+        if(this.state.mood === "happy") {
+            this.setState({
+                mood: "sad"
+            }) 
+        } else if (this.state.mood === "sad") {
+            this.setState({
+                mood: "happy"
+            }) 
+        }
+    }
+
+    render() {
+        return (
+        <div onClick={this.handleClick}>{this.state.mood}</div>
+        )
+    }
+}
+
+
+
+export default SimpleComponent
--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,10 @@
 // Code SimplerComponent Here
+import React from 'react'
+//Not extending this functional component so don't need {Component}
+
+//props which is the handleClick property passed from index.js
+//no class in this functional component means no use of "this" to reference the class
+const SimplerComponent = props => 
+<div onClick={props.handleClick}>I am just happy.</div>
+
+export default SimplerComponent
